0dec51be9a0ba2a56fefb4cdff073b67e98a494a
Özkan ŞEN <ozkansen@gmail.com>
2020-01-04 00:50:05 +0300

    heroku settings required $PORT enviroment

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-gunicorn --pythonpath=api-server -w $(nproc) main:app -b 0.0.0.0:5000 -k "egg:meinheld#gunicorn_worker" --log-level warning --log-file -
+gunicorn --pythonpath=api-server -w $(nproc) main:app -b 0.0.0.0:$PORT -k "egg:meinheld#gunicorn_worker" --log-level warning --log-file -
